91f851dfc3c0a482841e8f56e5396c66b472b267
caroline.lima <caroline.lima@kinghost.com.br>
2024-03-21 18:18:07 -0300

    Inserindo projeto base de Spring Boot

---
+++ innovix/src/test/java/lojaFisica/innovix/InnovixApplicationTests.java
@@ -0,0 +1,13 @@
+package lojaFisica.innovix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
